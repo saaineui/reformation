430a30067ffc80ec097a5b5371d7262f6638cdad
saaineui <saaineui@gmail.com>
2017-08-16 15:06:24 -0700

    Issue #5: remove remember_digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,46 +10,45 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170122031914) do
+ActiveRecord::Schema.define(version: 20170816220307) do
 
   create_table "submissions", force: :cascade do |t|
-    t.string   "source"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.integer  "web_form_id"
+    t.string "source"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "web_form_id"
     t.index ["web_form_id"], name: "index_submissions_on_web_form_id"
   end
 
   create_table "submissions_entries", force: :cascade do |t|
     t.integer "web_form_field_id"
     t.integer "submission_id"
-    t.string  "value"
+    t.string "value"
     t.index ["submission_id"], name: "index_submissions_entries_on_submission_id"
     t.index ["web_form_field_id"], name: "index_submissions_entries_on_web_form_field_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at",                         null: false
-    t.datetime "updated_at",                         null: false
-    t.string   "password_digest"
-    t.string   "remember_digest"
-    t.boolean  "admin",              default: false
-    t.string   "token"
-    t.string   "token_confirmation"
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.boolean "admin", default: false
+    t.string "token"
+    t.string "token_confirmation"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   create_table "web_form_fields", force: :cascade do |t|
     t.integer "web_form_id"
-    t.string  "name"
-    t.boolean "required",    default: false
+    t.string "name"
+    t.boolean "required", default: false
     t.index ["web_form_id"], name: "index_web_form_fields_on_web_form_id"
   end
 
   create_table "web_forms", force: :cascade do |t|
-    t.string  "name"
+    t.string "name"
     t.integer "user_id"
     t.index ["user_id"], name: "index_web_forms_on_user_id"
   end
